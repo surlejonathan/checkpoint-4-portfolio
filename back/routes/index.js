f4f4362a44b00b274917cda834a55c55115a2c54
surlejonathan <surle.jonathan@gmail.com>
2021-02-12 22:34:57 +0100

    add get route in index.js

--- back/routes/index.js
+++ back/routes/index.js
@@ -7,4 +7,8 @@ const project = require("./project");
 router.use("/admin", admin);
 router.use("/project", project);
 
+router.get("/", (req, res) => {
+  res.status(200).json("Hello World");
+});
+
 module.exports = router;
